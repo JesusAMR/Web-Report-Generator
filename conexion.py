c878426391d5823181fdd900760ae8c252cc1b72
JesusAMR <jesusmirr@gmail.com>
2017-11-17 18:23:48 -0600

    BaseC

--- conexion.py
+++ conexion.py
@@ -1,4 +1,21 @@
-#!/Python27/bin
+import mysql.connector
 
 class Conexion():
+    credentials = {'user':'jesus','password':'1234','host':'localhost'}
     def __init__():
+
+    def connect():
+        try:
+            cnx = mysql.connector.connect(**credentials)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        else:
+            cnx.close()
+    def close():
+        cnx.close()
+
